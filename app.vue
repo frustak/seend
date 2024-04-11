ed993707134ee0716f478ba2e50beedf4850912c
frustak <soulburn007@gmail.com>
2024-04-11 12:41:03 +0330

    starting :rock:

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <main
+    class="min-h-screen font-sans text-neutral-7 flex flex-col justify-center bg-secondary/50 p-10"
+  >
+    <section class="max-h-4xl grid grid-cols-2 grow bg-white rounded-6 p-2">
+      <div></div>
+      <button class="bg-secondary/90 text-primary rounded-6 text-50 uppercase">
+        Send
+      </button>
+    </section>
+  </main>
+</template>
+
+<style>
+@import "@unocss/reset/tailwind-compat.css";
+</style>
